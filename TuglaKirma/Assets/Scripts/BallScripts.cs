71e4b427d8782c74fb05106bfd6cc3e6b0afd5ca
emirselcukJr <emirselcukjr@gmail.com>
2023-03-31 18:28:02 +0300

    topun başlangıç ayarları

--- TuglaKirma/Assets/Scripts/BallScripts.cs
+++ TuglaKirma/Assets/Scripts/BallScripts.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BallScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    float speed = 700f;
+
+    [SerializeField]
+    bool inGame;
+
+    [SerializeField]
+    Transform ballStartPos;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (inGame == false)
+        {
+            transform.position = ballStartPos.position;
+        }
+
+
+
+
+        //topun paddle daki hareketi
+        if (Input.GetButtonDown("Jump") && !inGame)
+        {
+            inGame = true;
+            rb.AddForce(Vector2.up * speed);
+        }
+
     }
 }
